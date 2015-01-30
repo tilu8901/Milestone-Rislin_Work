3815a73f3c7460c3d1517c5896ea87f1c419b161
tilu8901 <tilu8901@uni.sydney.edu.au>
2015-01-30 18:09:53 +1100

    update

--- Booking Page/createClient.php
+++ Booking Page/createClient.php
@@ -4,36 +4,39 @@ $key = array_key_exists('key', $_GET) ? $_GET['key'] : null;
 $user = array_key_exists('user', $_GET) ? $_GET['user'] : null;
 
 if($key!=null && $user!=null){
-	$file = 'js/tableApp_tab.js';
-	$newfile = $user.'/tableApp_tab.js';
+	
 	//make directory
+	
 	mkdir($user, 0777, true);
-
 	
-	function recurse_copy($src,$dst) { 
-		$dir = opendir($src); 
-		@mkdir($dst); 
-		while(false !== ( $file = readdir($dir)) ) { 
-			if (( $file != '.' ) && ( $file != '..' )) { 
-				if ( is_dir($src . '/' . $file) ) { 
-					recurse_copy($src . '/' . $file,$dst . '/' . $file); 
-				} 
-				else { 
-					copy($src . '/' . $file,$dst . '/' . $file); 
-				} 
-			} 
-		} 
-		closedir($dir); 
-	} 
-	recurse_copy('book',$user);
+	$file = 'book/js/tableApp_tab.js';
+	$newfile = $user.'/tableApp_tab.js';
+	if (!copy($file, $newfile)) {
+		header('HTTP/1.1 500 Internal Server Error');
+		echo "failed to copy $file...\n";
+		return;
+	}
+	$file = 'book/timetable_tab.html';
+	$newfile = $user.'/timetable_tab.html';
+	if (!copy($file, $newfile)) {
+		header('HTTP/1.1 500 Internal Server Error');
+		echo "failed to copy $file...\n";
+		return;
+	}
 	
-	$fp=fopen($user.'/js/tableApp_tab.js','c');
-	fseek($fp,0,SEEK_END);
-	fwrite($fp, "var userKey ='".$key."';");
+	//insert key to tableApp_tabs.js
+	
+	$fp=fopen($user.'/tableApp_tab.js','c');
+	fseek($fp,420);
+	fwrite($fp, "var KEY ='".$key."';");
 	fclose($fp);
-	
+	header("HTTP/1.1 200 OK");
+	echo 'User Folder '.$user.' Created.';
 }
-else echo 'Must provide key and user';
+else{
+	header('HTTP/1.1 500 Internal Server Error');
+	echo 'Must provide key and user';
+} 
 
 
 ?>
